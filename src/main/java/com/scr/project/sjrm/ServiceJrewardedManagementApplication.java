4b3d425af80f348975040781c80e547a0d5f9765
scroutchy <stef.crombez@gmail.com>
2025-06-10 18:01:39 -0300

    feat: Initial commit

---
+++ src/main/java/com/scr/project/sjrm/ServiceJrewardedManagementApplication.java
@@ -0,0 +1,13 @@
+package com.scr.project.sjrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceJrewardedManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceJrewardedManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scr/project/sjrm/ServiceJrewardedManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scr.project.sjrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceJrewardedManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
